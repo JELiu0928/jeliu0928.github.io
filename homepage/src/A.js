8d3c7eb48482a9111c8727810f3bebb31da4a533
jeLiu <pig3703030@gmail.com>
2024-10-05 22:07:28 +0800

    react init

---
+++ homepage/src/A.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import test from "./A.module.css";
+
+const A = () => {
+    return (
+        <div>
+            <p className={test.p1}>我是A组件</p>
+        </div>
+    );
+};
+
+export default A;
---
+++ homepage/src/B.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const B = () => {
+    console.log("B組件重新渲染")
+    return (
+        <div>我是B组件</div>
+    );
+};
+
+export default B;
